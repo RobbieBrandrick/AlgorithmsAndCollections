1846983c24a0c64c6cf18b8ae2016c9a7968bf16
Robbie Brandrick <me@robbiebrandrick.com>
2016-03-22 06:56:44 -0400

    Added Project Euler solutions for problem 24 and 25

---
+++ ProjectEuler/Problem 24 - Lexicographic permutations/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+A permutation is an ordered arrangement of objects.For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. 
+If all of the permutations are listed numerically or alphabetically, we call it lexicographic order.The lexicographic 
+permutations of 0, 1 and 2 are:
+
+012   021   102   120   201   210
+
+What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
+REFERENCE: https://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_24___Lexicographic_permutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, int[]> permutations = new Dictionary<int, int[]>();
+            int count = 0;
+            int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int n = a.Length;
+            int k;
+            int l;
+
+            while (true)
+            {
+
+                count++;
+                permutations.Add(count, a.ToArray());
+
+                //1. Find the largest index k such that a[k] < a[k + 1].If no such index exists, the permutation is the last permutation.
+                for (k = n - 2; k >= 0 && a[k] > a[k + 1]; k--) { }
+
+                if (k == -1)
+                    break;
+
+                //2. Find the largest index l greater than k such that a[k] < a[l].
+                for (l = n - 1; l > k && a[k] > a[l]; l--) { }
+
+                //3. Swap the value of a[k] with that of a[l].
+                Swap(ref a[k], ref a[l]);
+
+                //4. Reverse the sequence from a[k + 1] up to and including the final element a[n].
+                int len = n - 1;
+                int kk = k + 1;
+                while(len > kk)
+                {
+                    Swap(ref a[kk], ref a[len]);
+                    kk++;
+                    len--;
+                }
+
+            }
+
+            //Print answer
+            int[] answer = permutations[1000000];
+
+            foreach(int i in answer)
+            {
+                Console.Write(i + ", ");
+            }
+
+        }
+
+        /// <summary>
+        /// Swaps int references from the lhs to the rhs using exclusive or swap
+        /// </summary>
+        private static void Swap(ref int lhs, ref int rhs)
+        {
+            lhs = rhs ^ lhs;
+            rhs = rhs ^ lhs;
+            lhs = rhs ^ lhs;
+        }
+
+    }
+}
---
+++ ProjectEuler/Problem 25 - 1000-digit Fibonacci number/Program.cs
@@ -0,0 +1,84 @@
+﻿/*
+The Fibonacci sequence is defined by the recurrence relation:
+
+Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+Hence the first 12 terms will be:
+
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits? 4782
+*/
+
+using System;
+using System.Numerics;
+using System.Diagnostics;
+
+namespace Problem_25___1000_digit_Fibonacci_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BigInteger max = BigInteger.Pow(10, 999);
+            BigInteger i = 0;
+            BigInteger answer = 0;
+
+            Stopwatch solutionStopWatch = Stopwatch.StartNew();
+            solutionStopWatch.Start();
+            do
+            {
+                i++;
+
+                Stopwatch fibStopWatch = Stopwatch.StartNew();
+
+                fibStopWatch.Start();
+                answer = Fibonacci(i);
+                fibStopWatch.Stop();
+                long elapsedFibTime = fibStopWatch.ElapsedTicks;
+
+                //Console.WriteLine("F{0}:{1} in {2} ticks", i, answer, elapsedTime);
+
+            } while (answer < max);
+
+            solutionStopWatch.Stop();
+            long elapsedSolutionTime = solutionStopWatch.ElapsedTicks;
+
+            Console.WriteLine("F{0}:{1} in {2} ticks", i, answer, elapsedSolutionTime);
+
+        }
+
+        private static BigInteger Fibonacci(BigInteger n)
+        {
+
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            BigInteger result = 0;
+            BigInteger previous = 1;
+            BigInteger previousPrevious = 0;
+
+            for(int i = 2; i <= n; i++)
+            {
+                result = previous + previousPrevious;
+                previousPrevious = previous;
+                previous = result;
+            }
+
+            return result;
+
+        }
+    }
+}
