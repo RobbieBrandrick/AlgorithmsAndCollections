9f21660838421865a990f6209a28cfe9ff1888ad
Robbie Brandrick <me@robbiebrandrick.com>
2016-04-03 11:16:43 -0400

    Added tests for FactorsBruteForce

--- Algorithms.Tests/AlgorithmsFactorizationTests.cs
+++ Algorithms.Tests/AlgorithmsFactorizationTests.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Xunit;
 
 namespace Algorithms.Tests
 {
     public class AlgorithmsFactorizationTests
     {
+
+        public class FactorsBruteForce
+        {
+
+            [Fact]
+            public void MustReturnTheFactorsOfTen()
+            {
+
+                List<long> expectedResult = new List<long>() { 1, 2, 5, 10 };
+                List<long> factors = Algorithms.FactorsBruteForce(10);
+
+                Assert.Equal(expectedResult, factors);
+
+            }
+
+        }
+
     }
 }
